8b13e207fa00928ca13f28b8d1acd9e8d5622e5c
phuc <phuc1603>
2021-05-29 16:55:43 +0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const app = express();
-const PORT = 3000;
+const morgan = require("morgan");
 
+const PORT = 3000;
+const app = express();
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
     res.send("Page INDEX");
-})
-
+});
 app.get("/trang-chu", (req, res) => {
     res.send("Page trang chu");
-})
+});
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`App is listening at http://localhost:${PORT}`);
 });
